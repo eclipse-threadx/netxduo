cb8dc31b2e7081e542c002232427378ca53f6f8a
Wenhui Xie <wenxi@microsoft.com>
2023-11-28 02:39:04 +0000

    Enable all the build configurations.

--- scripts/build_nxd.sh
+++ scripts/build_nxd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-$(dirname `realpath $0`)/../test/cmake/netxduo/run.sh build default_build_coverage v4_full_build v6_full_build
+$(dirname `realpath $0`)/../test/cmake/netxduo/run.sh build all
