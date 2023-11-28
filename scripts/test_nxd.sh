9732b61ee55b81d504f2b9dc992ef62f6a6d31c9
Wenhui Xie <wenxi@microsoft.com>
2023-11-28 02:39:04 +0000

    Enable all the build configurations.

--- scripts/test_nxd.sh
+++ scripts/test_nxd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-CTEST_PARALLEL_LEVEL=4 $(dirname `realpath $0`)/../test/cmake/netxduo/run.sh test default_build_coverage v4_full_build
+CTEST_PARALLEL_LEVEL=4 $(dirname `realpath $0`)/../test/cmake/netxduo/run.sh test all
