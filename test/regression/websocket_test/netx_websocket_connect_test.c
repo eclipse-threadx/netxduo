3ac76718c4f78dee42c53fdee751d3888d8fa5ac
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2025-10-01 17:58:58 +0100

    Fixed call to _client_secure_connect.

--- test/regression/websocket_test/netx_websocket_connect_test.c
+++ test/regression/websocket_test/netx_websocket_connect_test.c
@@ -351,6 +351,7 @@ UINT            code;
                                                 TEST_HOST_NAME, sizeof(TEST_HOST_NAME) - 1,
                                                 (UCHAR *)TEST_URI_PATH, sizeof(TEST_URI_PATH) - 1,
                                                 (UCHAR *)TEST_PROTOCOL, sizeof(TEST_PROTOCOL) - 1,
+                                                (UCHAR *)TEST_BEARER, sizeof(TEST_BEARER) -1,
                                                 NX_WAIT_FOREVER);
     if (status || client_websocket.nx_websocket_client_mutex.tx_mutex_ownership_count != 0)
         SET_ERROR_COUNTER(&error_counter, __FILE__, __LINE__);
